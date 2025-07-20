bc023b1d8bcfbfabc32e1dcc2f3a83d245117c08
Michelle Yang <yangmichelle1121@gmail.com>
2025-07-20 01:44:46 -0400

    Prompt user input to generate resize canvas
    Get input size of new canvas, remove row from container, generate new etchASketch game.

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i ++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
 
-    for (let m = 0; m < 16; m ++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        //grid.textContent = "hello";
-        row.appendChild(grid);
-    }
-    
-}
-
-// Hovering
-const grids = document.querySelectorAll(".grid");
-grids.forEach((grid) => {
-    grid.addEventListener("mouseenter", () => {
-        grid.classList.add("color");
+// Reset canvas size
+// Create button
+const button = document.querySelector("button");
+let size = 16;
+button.addEventListener("click", () => {
+    size = prompt("Please enter number of squares per size for new canvas (max 100):", 16);
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        container.removeChild(row);
     });
+    etchASketch();
 });
 
-//const button = document.createElement("button");
-//"Please enter number of squares per size for new canvas (max 100):"
+function etchASketch() {
+    for (let i = 0; i < size; i ++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (let m = 0; m < size; m ++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            //grid.textContent = "hello";
+            row.appendChild(grid);
+        }
+        
+    }
+    // Hovering
+    let grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseenter", () => {
+            grid.classList.add("color");
+        });
+    });
+};
+etchASketch();
+
 
